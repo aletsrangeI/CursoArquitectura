f70be0fbf29fd3c76fa82228f13283a89f710031
Alejandro Rangel <alejandro.rangel@grupobituaj.com.mx>
2023-07-31 16:03:12 -0600

    Carga inicial ora si

---
+++ Empresa.Ecommerce.Infrastructure.Interface/ICustomersRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Empresa.Ecommerce.Domain.Entity;
+using System.Threading.Tasks;
+
+namespace Empresa.Ecommerce.Infrastructure.Interface
+{
+    public interface ICustomersRepository
+    {
+        #region Metodos Sincronos
+        bool Insert(Customers customers);
+        bool Update(Customers customers);
+        bool Delete(string customerId);
+        Customers Get(string customerId);
+        IEnumerable<Customers> GetAll();
+        #endregion
+
+        #region Metodos Asincronos
+        Task<bool> InsertAsync(Customers customers);
+        Task<bool> UpdateAsync(Customers customers);
+        Task<bool> DeleteAsync(string customerId);
+        Task<Customers> GetAsync(string customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+        #endregion
+    }
+}
---
+++ Empresa.Ecommerce.Transversal.Common/IConnectionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+
+namespace Empresa.Ecommerce.Transversal.Common
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection GetConnection { get; }
+    }
+}
